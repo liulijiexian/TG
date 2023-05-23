af95d7369718891204e4bf805d6b89c3fc4ef41c
liulijiexian <tushanjiexian@gmail.com>
2023-05-23 21:05:33 +0800

    后端第一次上传

---
+++ tg/app.py
@@ -0,0 +1,132 @@
+from flask import Flask, request, url_for, redirect
+from flask_cors import CORS  # 跨域设置
+
+from project_code.music_code import Controller as music_Controller
+from project_code.mongodb_code import Controller as mongodb_Controller
+
+import json
+
+app = Flask(__name__)
+
+# 全局的api接口配置
+CORS(app,
+     supports_credentials=True)  # 默认情况下，不允许跨站点提交Cookie，如果你希望服务器允许用户跨源发出Cookie或经过身份验证的请求，那只需把supports_credentials 设置为True即可
+
+music_controller = music_Controller()
+mongodb_controller = mongodb_Controller('mongoFlaskVue', 'userInfo').musicFlakeVue
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return '资源不存在404'
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return '遇到意外的情况并阻止其执行请求500'
+
+
+@app.route('/getMusicMain', methods=['GET'])
+def getMusic():  # 获取歌曲列表
+    key = request.args.get('key')
+    pn = request.args.get('pn')
+    rn = request.args.get('rn')
+
+    if pn == '0':
+        pn = '1'
+
+    if key:
+        content = music_controller.musicListMain(key, pn, rn)
+        return json.dumps({'data': content, 'status': 200})
+    return json.dumps({'data': [], 'status': 404})
+
+
+@app.route('/getMusicUrl', methods=['GET'])
+def getMusicUrl():  # 获取指定歌曲
+    rid = request.args.get('rid')
+
+    if rid:
+        url = music_controller.getMusicMain(rid)
+        return json.dumps({'url': url, 'status': 200})
+    return json.dumps({'url': '', 'status': 404})
+
+
+@app.route('/getMusicMainLrc', methods=['GET'])
+def getMusicMainLrc():  # 获取歌词，只能中国大陆IP
+    musicId = request.args.get('musicId')
+
+    if musicId:
+        lrclist = music_controller.getMusicMainLrc(musicId)
+        return json.dumps({'lrclist': lrclist, 'status': 200})
+    return json.dumps({'lrclist': '', 'status': 404})
+
+
+@app.route('/login', methods=['POST'])
+def login():  # 登录
+    if request.content_type.startswith('application/json'):
+        # comment = request.get_json()["content"]
+        user = request.json.get('user')
+    elif request.content_type.startswith('multipart/form-data'):
+        user = request.form.get('user')
+    else:
+        user = request.values.get("user")
+
+    if user:
+        user_data = mongodb_controller.get_one(user)
+        if len(user_data) != 0:
+            return json.dumps({'userData': user_data, 'status': 200})
+    return register_from(user)  # 自动注册
+
+
+# 注册
+def register_from(user):
+    user_data = mongodb_controller.insert_one(user, [])
+    if len(user_data) != 0:
+        return json.dumps({'userData': user_data, 'status': 200})
+    return json.dumps({'userData': '', 'status': 404})
+
+
+@app.route('/register', methods=['POST'])
+def register():  # 注册
+    if request.content_type.startswith('application/json'):
+        # comment = request.get_json()["content"]
+        user = request.json.get('user')
+    elif request.content_type.startswith('multipart/form-data'):
+        user = request.form.get('user')
+    else:
+        user = request.values.get("user")
+
+    if user:
+        return register_from(user)
+    return json.dumps({'userData': '', 'status': 404})
+
+
+@app.route('/historyRecord', methods=['put'])
+def history_record():  # 修改历史记录
+    if request.content_type.startswith('application/json'):
+        # comment = request.get_json()["content"]
+        user = request.json.get('user')
+        data = request.json.get('data')
+    elif request.content_type.startswith('multipart/form-data'):
+        user = request.form.get('user')
+        data = request.form.get('data')
+    else:
+        user = request.values.get("user")
+        data = request.values.get('data')
+
+    if user:
+        if (data == 'delete'):
+            result = mongodb_controller.update_one(user, [])
+        else:
+            user_data = mongodb_controller.get_one(user)
+            if (len(user_data) != 0):
+                user_data = user_data['data']
+                user_data.append(data)
+                mongodb_controller.update_one(user, user_data)
+                return json.dumps({'status': 200, 'result': 1})
+    return json.dumps({'status': 404, 'result': 0})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9090)
+    # app.run(debug=True, host='127.0.0.1', port=9090)
---
+++ tg/project_code/music_code.py
@@ -0,0 +1,141 @@
+import requests
+import copy
+
+
+''' 公共请求头  '''
+class Headers:
+    headers = {
+        'UserAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+    }
+
+    session = requests.session()
+
+
+''' 获取音乐地址模块 '''
+class GetMusic(Headers):
+    # 初始化
+    def __init__(self):
+        self.__headers = copy.deepcopy(self.headers)
+
+        self.__session = self.session
+
+    # get请求，获取歌曲
+    def get(self, mid, url='http://www.kuwo.cn/api/v1/www/music/playUrl', **kwargs):
+        params = {
+            'mid': mid,  # 音乐id
+            'type': 'convert_url3',  # 类型，不用改
+            # 'httpsStatus': '1',
+            'br': '320kmp3',  # 在线音乐的比特率，越大则音质越高，可选的有 128kmp3、 192kmp3 和 320kmp3
+        }
+        return self.__session.get(url, params=params, headers=self.__headers, **kwargs).json()
+
+    # 获取歌词lrc文件
+    def getlrc(self, mid, url='http://m.kuwo.cn/newh5/singles/songinfoandlrc', **kwargs):
+        params = {
+            'musicId': mid,  # 音乐id
+            'httpsStatus': '1',
+        }
+        return self.__session.get(url, params=params, headers=self.__headers, **kwargs).json()
+
+    # 更新请求头
+    def upheaders(self, headers):
+        self.__headers = headers
+
+
+
+
+''' 获取歌曲列表 '''
+class MusicList(Headers):
+    def __init__(self):
+        self.__headers = copy.deepcopy(self.headers)
+
+        self.__params = {
+            'key': '',  # 歌名
+            'pn': 1,  # 页数
+            'rn': 30,  # 每一页获取的数据个数
+        }
+
+        self.__session = self.session
+        self.__url = 'http://www.kuwo.cn/api/www/search/searchMusicBykeyWord?'
+
+        self.__session = self.session
+
+
+    def get(self, key, pn=1, rn=30, **keyword):
+        self.upkey(key, pn, rn)
+
+        # 获取kw_token，反反爬措施
+        req = self.__session.get(f'http://www.kuwo.cn/search/list?key={key}', headers=self.__headers)
+        kw_token = req.cookies.get('kw_token')
+        headers = {}
+
+        headers['csrf'] = kw_token
+        headers['Cookie'] = 'kw_token=' + str(kw_token)
+        headers['Referer'] = req.url
+        self.upheaders(**headers)
+
+        return self.__session.get(self.__url, params=self.__params, headers=self.__headers, **keyword).json()['data']['list']
+
+    # 更新key
+    def upkey(self, key, pn=1, rn=30):
+        self.__params['key'] = key
+        self.__params['pn'] = pn
+        self.__params['rn'] = rn
+
+    # 更新请求头
+    def upheaders(self, headers=None, **kwargs):
+        if headers:
+            self.__headers = headers
+        else:
+            for j, k in kwargs.items():
+                self.__headers[j] = k
+
+    # 获取headers
+    def get_headers(self):
+        return self.__headers
+
+
+# 控制器
+class Controller:
+    def __init__(self):
+        self.__getMusic = GetMusic()
+        self.__musicList = MusicList()
+
+        # 存放音乐名、作者、音乐id 和 时长
+        self.__music = []
+
+    # 获取歌曲目录
+    def musicListMain(self, key, pn=1, rn=30):
+        # print("歌名\t作者\tid\t时长")
+        self.__music = []
+        for i in self.__musicList.get(key, pn, rn):
+            musicDict = {}
+            musicDict['musicimage'] = str(i['albumpic'])  # 歌曲图像
+            musicDict['name'] = str(i['name']).strip().replace('&nbsp;', ' ')  # 歌名
+            musicDict['author'] = str(i['artist']).strip().replace('&nbsp;', ' ')  # 作者
+            # musicDict['url'] = self.getMusicMain(i['rid'])  # 音乐url（一次性访问太多 酷我音乐 容易失败）
+            musicDict['url'] = i['rid']  # 音乐url
+            musicDict['time'] = str(i['songTimeMinutes']).strip().replace('&nbsp;', ' ')  # 时长
+
+            self.__music.append(musicDict)
+            # print(f'{i["name"]}\t{i["artist"]}\t{i["rid"]}\t{i["songTimeMinutes"]}')
+
+        return self.__music
+
+    # 获取歌曲对应链接
+    def getMusicMain(self, mid, url=None):
+        if url:
+            return self.__getMusic.get(mid, url)['data']['url']
+        return self.__getMusic.get(mid)['data']['url']
+
+    # 获取歌曲对应歌词
+    def getMusicMainLrc(self, mid, url=None):
+        if url:
+            lrclist = self.__getMusic.getlrc(mid, url)['data']['lrclist']
+        else:
+            lrclist = self.__getMusic.getlrc(mid)['data']['lrclist']
+        return lrclist
+
+if __name__ == "__main__":
+    controller = Controller()
+    print(controller.musicListMain(key='许嵩'))
