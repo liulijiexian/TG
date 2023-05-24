618271412cc2081d265c4882767fe93afe124ca5
liulijiexian <tushanjiexian@gmail.com>
2023-05-24 13:51:29 +0800

    前端第一次提交

--- vue-music/env.d.ts
+++ vue-music/env.d.ts
@@ -1,32 +1,3 @@
 /// <reference types="vite/client" />
-declare module 'APlayer' {
-    export default class APlayer {
-      constructor(options: APlayerOptions);
-    }
-    
-    interface APlayerOptions {
-      container: HTMLElement | null;
-      audio: AudioInfo[];
-      autoplay?: boolean;
-      loop?: 'all' | 'one' | 'none';
-      order?: 'list' | 'random';
-      volume?: number;
-      preload?: 'none' | 'metadata' | 'auto';
-      listFolded?: boolean;
-      listMaxHeight?: string;
-      lrcType?: number;
-      fixed?: boolean;
-      mini?: boolean;
-      mutex?: boolean;
-      theme?: '#b7daff' | '#efdcde' | '#b3e0ca' | '#cbbdeb' | '#ffd8b8' | '#78b8ff';
-    }
-  
-    interface AudioInfo {
-      name?: string;
-      artist?: string;
-      url: string;
-      cover?: string;
-      lrc?: string;
-    }
-}
+declare module 'APlayer';
   
